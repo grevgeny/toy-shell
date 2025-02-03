20287132f84cf3c52234948c655692947f78daa6
Evgenii Grigorenko <eu.grigorenko@gmail.com>
2025-02-03 17:22:56 +0400

    refactor: introduce `Command` enum and extract parsing & execution logic

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,75 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        let (cmd, args) = {
-            let input = input.trim();
-            input.split_once(' ').unwrap_or((input, ""))
+        let command = {
+            let trimmed_input = input.trim();
+            if trimmed_input.is_empty() {
+                continue;
+            }
+            parse_command(trimmed_input)
         };
 
-        match (cmd, args) {
-            ("echo", message) => println!("{}", message),
-            ("exit", "0") => std::process::exit(0),
-            ("type", "echo" | "exit" | "type") => println!("{args} is a shell builtin"),
-            ("type", unknown_cmd) => println!("{unknown_cmd}: not found"),
-            _ => println!("{}: command not found", input.trim()),
+        execute_command(command);
+    }
+}
+
+enum Command {
+    Echo(String),
+    Exit(i32),
+    Type(String),
+    Unknown(String),
+}
+
+fn is_builtin(cmd: &str) -> bool {
+    matches!(cmd, "echo" | "exit" | "type")
+}
+
+fn parse_command(input: &str) -> Command {
+    let mut tokens = input.split_whitespace();
+
+    let cmd = match tokens.next() {
+        Some(token) => token,
+        None => return Command::Unknown(String::new()),
+    };
+
+    match cmd {
+        "echo" => {
+            let message = tokens.collect::<Vec<_>>().join(" ");
+            Command::Echo(message)
         }
+        "exit" => {
+            if let Ok(code) = tokens.collect::<String>().parse::<i32>() {
+                Command::Exit(code)
+            } else {
+                Command::Unknown(input.to_string())
+            }
+        }
+        "type" => {
+            if let Some(cmd) = tokens.next() {
+                Command::Type(cmd.to_string())
+            } else {
+                Command::Unknown(input.to_string())
+            }
+        }
+        _ => Command::Unknown(cmd.to_string()),
+    }
+}
+
+fn execute_command(command: Command) {
+    match command {
+        Command::Echo(msg) => {
+            println!("{msg}");
+        }
+        Command::Exit(code) => std::process::exit(code),
+        Command::Type(cmd) if is_builtin(&cmd) => {
+            println!("{cmd} is a shell builtin");
+        }
+        Command::Type(cmd) => {
+            println!("{cmd}: not found")
+        }
+        Command::Unknown(cmd) if !cmd.is_empty() => {
+            println!("{cmd}: command not found");
+        }
+        Command::Unknown(_) => {}
     }
 }
